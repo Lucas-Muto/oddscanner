a5c9c7ef0d70d4a4cece23f96a535754f1fc733c
unknown <lucasmuto@id.uff.br>
2024-10-14 15:04:40 -0300

    novibet pronto

--- novibet.py
+++ novibet.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
+import subprocess
 
 # Caminho para o chromedriver (ajuste conforme necessário)
 chromedriver_path = r"C:\Users\Lucas\Desktop\chromedriver-win64\chromedriver.exe"
@@ -21,19 +22,43 @@ service = Service(chromedriver_path)
 # Inicializa o driver do Selenium com as opções definidas
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Lista de URLs dos campeonatos
-urls = [
+# Lista de URLs dos campeonatos da América do Sul
+urls_america_sul = [
     "https://br.novibet.com/apostas-esportivas/futebol/4372606/brazil/serie-a/5909324",  # Série A - Brasil
     "https://br.novibet.com/apostas-esportivas/futebol/4372606/brazil/copa-do-brasil/4381081",  # Copa do Brasil
     "https://br.novibet.com/apostas-esportivas/futebol/4372606/south-america/copa-sudamericana/4380718",  # Copa Sulamericana
     "https://br.novibet.com/apostas-esportivas/futebol/4372606/south-america/copa-libertadores/4380709"   # Copa Libertadores
 ]
 
-# Iniciar o conteúdo do arquivo com o título da região e o nome do site
+# Lista de URLs dos campeonatos da Europa
+urls_europa = [
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/champions-league/uefa-champions-league/5102048",  # Champions League
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/europa-conference-league/uefa-europa-conference-league/5102056",  # Conference League
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/europa-league/uefa-europa-league/4372887",  # Europa League
+    # Novos campeonatos
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/england/premier-league/5908949",  # Premier League
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/england/fa-cup/4373735",  # FA Cup
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/spain/laliga/5909177",  # La Liga
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/spain/copa-del-rey/4375978",  # Copa del Rey
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/italy/serie-a/5909241",  # Serie A
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/italy/coppa-italia/4376102",  # Coppa Italia
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/germany/bundesliga/5909087",  # Bundesliga
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/germany/dfb-pokal/4376067",  # DFB Pokal
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/france/ligue-1/5909322",  # Ligue 1
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/france/coupe-de-france/4376257"  # Coupe de France
+]
+
+# Lista de URLs dos campeonatos de seleções
+urls_selecoes = [
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/europe/uefa-nations-league/4373075",  # UEFA Nations League
+    "https://brasil.novibet.com/apostas-esportivas/futebol/4372606/world/world-cup-2026/4679255"  # Copa do Mundo 2026
+]
+
+# Iniciar o conteúdo do arquivo com os títulos das regiões e o nome do site
 output_content = "AMÉRICA DO SUL ( NOVIBET )\n\n"
 
 # Função para acessar cada campeonato e coletar as informações
-def scrape_championship(url):
+def scrape_championship(url, region_title):
     global output_content
     
     # Acessar a URL do campeonato
@@ -51,14 +76,15 @@ def scrape_championship(url):
     except Exception as e:
         pass  # Se não houver modal, seguir em frente
 
-    # Clicar na aba "Partidas" usando XPath com o texto "Partidas"
+    # Verificar se a aba "Partidas" está presente
     try:
         partidas_tab = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'couponSelections_subItem') and contains(., 'Partidas')]"))
+            EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'couponSelections_subItem') and contains(., 'Partidas')]"))
         )
         partidas_tab.click()
     except Exception as e:
-        pass  # Se não conseguir clicar, seguir em frente
+        print(f"A aba 'Partidas' não está disponível para {url}. Pulando este campeonato.")
+        return  # Pular para o próximo campeonato
 
     # Aguardar 5 segundos para as partidas serem carregadas
     time.sleep(1)
@@ -112,9 +138,23 @@ def scrape_championship(url):
         else:
             output_content += "Erro ao capturar times.\n"
 
-# Iterar sobre todos os links dos campeonatos
-for url in urls:
-    scrape_championship(url)
+# Iterar sobre os links dos campeonatos da América do Sul
+for url in urls_america_sul:
+    scrape_championship(url, "AMÉRICA DO SUL")
+
+# Adicionar um título para os campeonatos da Europa
+output_content += "\nEUROPA ( NOVIBET )\n\n"
+
+# Iterar sobre os links dos campeonatos da Europa
+for url in urls_europa:
+    scrape_championship(url, "EUROPA")
+
+# Adicionar um título para os campeonatos de seleções
+output_content += "\nSELEÇÕES ( NOVIBET )\n\n"
+
+# Iterar sobre os links dos campeonatos de seleções
+for url in urls_selecoes:
+    scrape_championship(url, "SELEÇÕES")
 
 # Fechar o driver no final
 driver.quit()
